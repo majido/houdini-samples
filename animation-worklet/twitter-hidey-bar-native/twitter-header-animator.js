b704a1a806c957ed60c6520be74b7791976d2c5f
Majid Valipour <majidvp@chromium.org>
2017-11-06 15:50:22 -0500

    clean up hidey bar

--- animation-worklet/twitter-hidey-bar-native/twitter-header-animator.js
+++ animation-worklet/twitter-hidey-bar-native/twitter-header-animator.js
@@ -17,54 +17,70 @@ function sign(value) {
   return value < 0 ? -1 : 1;
 }
 
-var MIN_HIDE_AMOUNT = -50;
-var MAX_HIDE_AMOUNT = 0;
+function clamp(value, min, max) {
+  return Math.max(min, Math.min(max, value));
+}
+
+var BAR_HEIGHT = 190
+var MIN_HIDE_AMOUNT = -1 * BAR_HEIGHT; // fully hidden
+var MAX_HIDE_AMOUNT = 0;               // fully shown
 var HIDE_SPEED = 0.35;
+var TIME_DELTA = 16;
+
+// number of frames to wait before considering scroll to be inactive.
+// This is a short-term hack until we have proper ScrollTimline.phase.
+var FRAMES_TO_WAIT = 8;
 
 registerAnimator('twitter-header', class TwitterHeader {
   constructor() {
     this.hideAmount_ = 0;
-    this.hideChange_ = 0;
+    this.lastDirection_ = 1;
     this.lastScrollPos_ = -1;
+    this.countIdleFrames = 0;
   }
 
   animate(currentTime, effect) {
     var scrollPos = currentTime;
-    var timeDelta = 16;
-    var scrollPhase = 'active';
-    if (scrollPos == this.lastScrollPos_)
+
+    // Drive animation based on scroll delta.
+    var lastScroll = (this.lastScrollPos_ - scrollPos);
+    this.lastScrollPos_ = scrollPos;
+
+    // Wait for N frames of no scroll before assuming idle.
+    if (lastScroll == 0) {
+      this.countIdleFrames++;
+    } else {
+      this.countIdleFrames = 0;
+      this.lastDirection_ = sign(lastScroll);
+    }
+
+    var scrollPhase;
+    if (this.countIdleFrames == 0)
+      scrollPhase = 'active';
+    else if (this.countIdleFrames < FRAMES_TO_WAIT)
+      scrollPhase = 'waiting';
+    else
       scrollPhase = 'idle';
 
 
-    // We don't want the header to hide (or ever go above 189).
-    var currentMinHideAmount = Math.max(Math.min(0, 189 - scrollPos), MIN_HIDE_AMOUNT);
-
-    if (scrollPhase == 'idle') {
-      // When the scroll goes idle, determine if we need to keep sliding the header.
-      if (this.hideAmount_ != currentMinHideAmount && this.hideAmount_ != MAX_HIDE_AMOUNT) {
-        this.hideAmount_ += this.hideChange_ * timeDelta;
-        this.hideAmount_ = Math.max(currentMinHideAmount, Math.min(MAX_HIDE_AMOUNT, this.hideAmount_));
-      }
+    switch(scrollPhase){
+      case 'idle':
+        // drive animation based on time delta
+        // TODO: We should probably animate to hide/show based on how much the
+        // bar is visibile instead of the direction of the movement.
+        this.hideAmount_ += this.lastDirection_ * HIDE_SPEED  * TIME_DELTA;
+        break;
+      case 'active':
+         // drive animation based on scroll delta
+        this.hideAmount_ += lastScroll;
+        break;
     }
 
-    if (scrollPos != this.lastScrollPos_) {
-      // Track the last scroll and
-      var lastScroll = (this.lastScrollPos_ - scrollPos);
-      var direction = sign(lastScroll);
-      var speed = HIDE_SPEED;
-      this.hideChange_ = speed * direction;
-      this.hideAmount_ += lastScroll;
-      this.hideAmount_ = Math.max(currentMinHideAmount, Math.min(MAX_HIDE_AMOUNT, this.hideAmount_));
-    }
+    var baseline = clamp(-scrollPos, MIN_HIDE_AMOUNT, MAX_HIDE_AMOUNT);
+    this.hideAmount_ = clamp(this.hideAmount_, baseline, MAX_HIDE_AMOUNT);
 
-    // Avatar position
-    // effect.children[1].localTime = scrollPos > 189 - 45 * 0.6 ?
-    //     scrollPos - (189 - 45 * 0.6) + 45 + this.hideAmount_ : 45 + this.hideAmount_;
-
-    console.log(scrollPos + "->" + this.hideAmount_)
-    // Header bar position
+     // Position header bar according to scroll position and hide amount.
     effect.localTime = 0.001 *  (scrollPos + this.hideAmount_);
 
-    this.lastScrollPos_ = scrollPos;
   }
 });
