37bcaf413322379efc8eb7138a2eb1a7197a9d25
Majid Valipour <majidvp@chromium.org>
2019-05-02 11:22:12 -0400

    Working simple case

--- animation-worklet/touch-input/dnd-animator.js
+++ animation-worklet/touch-input/dnd-animator.js
@@ -9,9 +9,11 @@ registerAnimator(
       this.releaseBehavior = options.releaseBehavior;
 
       this.state_ = state || {
-        phase: "idle", // we have three phase: idle, finishing, tracking
+        // we have three phase: idle, finishing, tracking
+        phase: "idle",
         pointerOrigin: { x: 0, y: 0 },
-        localTimeOrigin: 0, // track localTime when we transition to idle to ensure the effect is persistent
+        // track localTime when we transition to idle to ensure the effect is persistent
+        localTimeOrigin: 0,
         lastMessage: ""
       };
     }
@@ -31,7 +33,8 @@ registerAnimator(
         const deltaX = event.screenX - this.state_.pointerOrigin.screenX;
         effect.localTime = this.state_.localTimeOrigin + deltaX;
         //console.log(deltaX, effect.localTime);
-      } else {
+      } else if (phase == "finishing") {
+      } else if (phase == "idle") {
         // TODO: make this time based animated
         effect.localTime = this.state_.localTimeOrigin;
       }
@@ -43,7 +46,10 @@ registerAnimator(
           if (this.state_.phase == "idle") {
             console.log("---- 👋 START ----");
 
-            this.state_.pointerOrigin = { screenX: event.screenX, screenY: event.screenY };
+            this.state_.pointerOrigin = {
+              screenX: event.screenX,
+              screenY: event.screenY
+            };
           }
           this.state_.phase = "tracking";
           break;
@@ -57,7 +63,9 @@ registerAnimator(
           this.state_.phase = "idle";
           this.state_.pointerOrigin = { x: 0, y: 0 };
           this.state_.localTimeOrigin = this.calculateFinalTarget(localTime);
-          console.log(`${this.releaseBehavior} @ ${this.state_.localTimeOrigin}`);
+          console.log(
+            `${this.releaseBehavior} @ ${this.state_.localTimeOrigin}`
+          );
 
           break;
       }
@@ -69,12 +77,16 @@ registerAnimator(
       const start = 0;
       // TODO: This really should be effect.getLocalTiming().duration
       const end = 200;
-      const middle = (end - start)/2;
-      switch(this.releaseBehavior) {
-        case 'remain':   return localTime;
-        case 'complete': return (localTime < middle) ? start : end;
-        case 'return':   return start;
-        default: throw 'Invalid releaseBehavior';
+      const middle = (end - start) / 2;
+      switch (this.releaseBehavior) {
+        case "remain":
+          return localTime;
+        case "complete":
+          return localTime < middle ? start : end;
+        case "return":
+          return start;
+        default:
+          throw "Invalid releaseBehavior";
       }
     }
 
@@ -84,4 +96,4 @@ registerAnimator(
   }
 );
 
-console.log('Worklet code was processed.');
+console.log("Worklet code was processed.");
